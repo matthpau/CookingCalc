c514c69434fd24a8fc58989a88e4727eeddec8d1
matthpau <matthpau@gmail.com>
2019-05-22 10:38:13 +0200

    Got my first form working

--- AppTimesCalc/urls.py
+++ AppTimesCalc/urls.py
@@ -4,7 +4,7 @@ from . import views
 #https://docs.djangoproject.com/en/2.2/topics/http/shortcuts/
 
 urlpatterns = [
-    path('', views.Calculator, name='calculator'),
+    path('', views.Calculator, name='Calculator'),
+    path('calc-result', views.CalcResult, name='CalcResult'),
     path('MealPlanner', views.MealPlanner, name='MealPlanner'),
-
 ]
--- AppTimesCalc/views.py
+++ AppTimesCalc/views.py
@@ -4,7 +4,12 @@ from .models import *
 def Calculator(request):
     meats = MeatType.objects.all()
     cooking_info = CookingInfo.objects.all()
-    return render(request, 'AppTimesCalc/CookingCalc.html', {'meats': meats, 'cooking_info': cooking_info})
+    context = {'meats': meats, 'cooking_info': cooking_info}
+    return render(request, 'AppTimesCalc/CookingCalc.html', context)
+
+def CalcResult(request):
+    context = {'calc_inputs': request.GET}
+    return render(request, 'AppTimesCalc/CookingCalcRes.html', context)
 
 def MealPlanner(request):
     return render(request, 'AppTimesCalc/MealPlanner.html')
