88d5256c365afea0c873f06abd262543478806cf
matthpau <matthpau@gmail.com>
2019-05-24 15:35:21 +0200

    First correct results!

--- AppTimesCalc/forms.py
+++ AppTimesCalc/forms.py
@@ -5,10 +5,10 @@ from django import forms
 from .models import *
 
 class CalcForm1(forms.Form):
-    MeatType = forms.ModelChoiceField(MeatType.objects.all(), label="Meat Type ", required=True, initial=1)
-    CookingLevel = forms.ModelChoiceField(CookingLevel.objects.all(), label="Cooking Level ", required = True, initial=1)
+    MeatType = forms.ModelChoiceField(MeatType.objects.all(), label="Meat Type ", required=True)
+    CookingLevel = forms.ModelChoiceField(CookingLevel.objects.all(), label="Cooking Level ", required = True)
     EatingTime = forms.TimeField(label="When do you want to eat? HH:MM  (optional)", required = False, initial="00:00")
-    Weight_kg = forms.DecimalField(label="Weight of meat (kilograms) ", initial=1, required = False)
+    Weight_kg = forms.DecimalField(label="Weight of meat (kilograms) ", initial=0, required = False)
     Weight_lb = forms.DecimalField(label="or, weight of meat (pounds) ", initial=0, required = False)
     Weight_g = forms.DecimalField(label="or, weight of meat (grams) ", initial=0, required = False)
 
@@ -25,8 +25,8 @@ class CalcForm1(forms.Form):
             raise forms.ValidationError("Please put a weight in one and only one box")
 
 class CalcForm2(forms.Form):
-    MeatType = forms.ModelChoiceField(MeatType.objects.all(), label="Meat Type ", required=True, initial=1)
-    CookingLevel = forms.ModelChoiceField(CookingLevel.objects.all(), label="Cooking Level ", required = True, initial=1)
+    MeatType = forms.ModelChoiceField(MeatType.objects.all(), label="Meat Type ", required=True)
+    CookingLevel = forms.ModelChoiceField(CookingLevel.objects.all(), label="Cooking Level ", required = True)
     EatingTime = forms.TimeField(label="When do you want to eat? HH:MM (optional)", required = False, initial="00:00")
     Count_adults = forms.IntegerField(label='Number of Adults', initial = 0, required = False, min_value=0)
     Count_children = forms.IntegerField(label='Number of Children', initial = 0, required = False, min_value=0)
