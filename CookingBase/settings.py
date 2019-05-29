9daa63d2b989582e589eff4b164c0ba34df83d0f
matthpau <matthpau@gmail.com>
2019-05-29 10:14:09 +0200

    ∆

--- CookingBase/settings.py
+++ CookingBase/settings.py
@@ -135,15 +135,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Berlin'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
