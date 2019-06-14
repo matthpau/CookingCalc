b415d91dce6849e2831c30477234337e6f68a10b
matthpau <matthpau@gmail.com>
2019-06-14 12:12:49 +0200

    ∆

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 # https://wsvincent.com/django-custom-user-model-tutorial/
@@ -15,3 +16,10 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
