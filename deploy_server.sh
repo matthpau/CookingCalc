ad7868ea29ba3a46f144ac74a7155f2cfc818b4d
matthpau <matthpau@gmail.com>
2019-06-23 15:55:17 +0200

    A good days work

--- deploy_server.sh
+++ deploy_server.sh
@@ -3,10 +3,11 @@ git pull https://matthpau:6N0k0vg6@github.com/matthpau/CookingCalc
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata dumps/Converter.json
+python manage.py loaddata dumps/MeatType.json
 python manage.py loaddata dumps/CookingLevel.json
 python manage.py loaddata dumps/CookingInfo.json
-python manage.py loaddata dumps/MeatType.json
+python manage.py loaddata dumps/Converter.json
+
 
 
 
