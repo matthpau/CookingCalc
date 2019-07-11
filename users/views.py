5b603ceeb43173ad3ec8c88dd834a2125738f451
matthpau <matthpau@gmail.com>
2019-07-11 18:12:08 +0200

    l

--- users/views.py
+++ users/views.py
@@ -4,21 +4,21 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 
 def update_profile(request):
-        if request.method == 'POST':
-            user_form = UserForm(request.POST, instance=request.user)
-            profile_form = ProfileForm(request.POST, instance=request.user.profile)
-            if user_form.is_valid() and profile_form.is_valid():
-                user_form.save()
-                profile_form.save()
-                messages.success(request, ('Your profile was successfully updated'))
-                return HttpResponseRedirect('/')
-            else:
-                messages.error(request, ('Please correct the error.'))
-        else: # GET
-            user_form = UserForm(instance=request.user)
-            profile_form = ProfileForm(instance=request.user.profile)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, ('Your profile was successfully updated'))
+            return HttpResponseRedirect('/')
+        else:
+            messages.error(request, ('Please correct the error.'))
+    else: # GET
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
 
-        return render(request, 'users/profile.html',{
-            'user_form': user_form,
-            'profile_form': profile_form
-        })
+    return render(request, 'users/profile.html',{
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
