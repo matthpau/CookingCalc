97df05361fc4253c5de3de93f8c626d060a6354a
Paul Matthews <paulmatthews@Pauls-MBP.fritz.box>
2019-07-30 17:35:14 +0200

    l

--- AppTimesCalc/management/commands/masterdata_import.py
+++ AppTimesCalc/management/commands/masterdata_import.py
@@ -16,3 +16,4 @@ class Command(BaseCommand):
         call_command('loaddata', 'dumps/CookingInfo.json')
         call_command('loaddata', 'dumps/Converter.json')
         call_command('loaddata', 'dumps/store_type.json')
+        call_command('loaddata', 'dumps/user_country.json')
